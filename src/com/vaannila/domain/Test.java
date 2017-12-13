b98bf51c72003ffb21146305cbb8783b440ce1ac
Jenkins <jenkins@ip-172-31-28-172.ap-south-1.compute.internal>
2017-12-13 23:43:31 +0000

    jenkinsfile

--- src/com/vaannila/domain/Test.java
+++ src/com/vaannila/domain/Test.java
@@ -1,12 +1,12 @@
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class UserTest {
+public class UserTest ( 
    User MyUser = new User(basil,DevOPS12345);
 
    @Test
    public void testgetName {
 	assertEquals(MyUser.getName(), basil);
    }
-}
+)
 
